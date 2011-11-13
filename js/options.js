9ea741903cea7377f0ff5d9ed0842049ab25d560
Gaelian Ditchburn <gaelian.ditchburn@gmail.com>
2011-11-13 14:04:28 +1100

    If username is cleared from Options page, all user details are now cleared.

--- js/options.js
+++ js/options.js
@@ -11,28 +11,29 @@ function init()
 
 	$success.hide();
 	$username.val(getItem('username'));
-	
+
 	if (getItem('useSsl') == 'true')
 	{
 		$useSsl.attr('checked', true);
 	}
 	else
 	{
-		$useSsl.attr('checked', false);	
+		$useSsl.attr('checked', false);
 	}
-	
 
 	$('#save').click(function(){
 		if ($username.val().length > 0)
 		{
-			setItem('username', $username.val().toLowerCase());			
+			setItem('username', $username.val().toLowerCase());
+			$useSsl.attr('checked') ? setItem('useSsl', true) : setItem('useSsl', false);
 		}
 		else
 		{
 			removeItem('username');
+			removeItem('userId');
+			removeItem('useSsl');
 		}
 
-		$useSsl.attr('checked') ? setItem('useSsl', true) : setItem('useSsl', false);
 		$success.show().fadeOut(2000);
 	});
 }
